3c8a471419d5e422191b02ff995b42f295cc7283
Morten Andersson <morten.andersson@gmail.com>
2024-04-09 23:31:27 +0200

    shuffle songs

--- scripts/world.gd
+++ scripts/world.gd
@@ -12,16 +12,18 @@ var music= [
 	preload("res://music/Pixelated Dreams.mp3"),
 	preload("res://music/Pixelated Dreams2.mp3"),
 ]
+var current_song:=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize();
+	music.shuffle();
 	music_player.connect("finished", _on_loop_sound)
 	_on_loop_sound()
 
 func _on_loop_sound():
-	var oldstream = music_player.stream
-	while(oldstream == music_player.stream):
-		music_player.stream = music.pick_random()
+	music_player.stream = music[current_song]
+	current_song = (current_song +1) % music.size()
 	music_player.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
