6970f5fe1fa80d0e5103e4a05b14055b1ee4fa9e
Morten Andersson <morten.andersson@gmail.com>
2024-04-09 23:26:06 +0200

    added more music, music randomly plays new song when current ends

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
-@onready var music = %AudioStreamPlayer
+@onready var music_player = %AudioStreamPlayer
+
+var music= [
+	preload("res://music/Neon Sorcery.mp3"),
+	preload("res://music/Neon Sorcery2.mp3"),
+	preload("res://music/Neon Wizard.mp3"),
+	preload("res://music/Neon Wizard2.mp3"),
+	preload("res://music/Neon Uprising.mp3"),
+	preload("res://music/Neon Uprising2.mp3"),
+	preload("res://music/Pixelated Dreams.mp3"),
+	preload("res://music/Pixelated Dreams2.mp3"),
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	music.play()
+	music_player.connect("finished", _on_loop_sound)
+	_on_loop_sound()
 
+func _on_loop_sound():
+	var oldstream = music_player.stream
+	while(oldstream == music_player.stream):
+		music_player.stream = music.pick_random()
+	music_player.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
