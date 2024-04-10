f3b69764229a91eafafdf60e8711418989a9566d
Morten Andersson <morten.andersson@gmail.com>
2024-04-10 15:08:09 +0200

    proper scaling

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,9 +5,10 @@ const SPEED = 100.0
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
-	if (velocity.x > 0.1):
-		sprite.flip_h = true
-	elif (velocity.x < -0.1):
-		sprite.flip_h = false
-		
+
 	move_and_slide()
+	# Check for horizontal movement
+	if direction.x > 0:
+		sprite.scale.x = -1  # Assuming your sprite faces left by default
+	elif direction.x < 0:
+		sprite.scale.x = 1
