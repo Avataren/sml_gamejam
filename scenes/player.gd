40b6066863f8731608e97619a2152644b9d5fc4f
Morten Andersson <morten.andersson@gmail.com>
2024-04-13 15:59:19 +0200

    ignore tmp files

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 @onready var sprite = %Sprite2D
+@onready var spellbook:Spellbook
+@export var starting_spells:Array[SpellResource]
 
 var missile:PackedScene = load("res://spells/missile.tscn")
 var missile_timer := Timer.new()
@@ -12,13 +14,20 @@ var alive = true
 @onready var player_area:Area2D = %PlayerBoundsArea
 func _ready():
 	Global.player = self
-	add_child(missile_timer)
-	missile_timer.wait_time = 0.5
-	missile_timer.one_shot = false
-	missile_timer.start()
-	missile_timer.timeout.connect(_shoot_missile)
+	#add_child(missile_timer)
+	#missile_timer.wait_time = 0.5
+	#missile_timer.one_shot = false
+	#missile_timer.start()
+	#missile_timer.timeout.connect(_shoot_missile)
 	%ProgressBar.max_value = max_hp
 	%ProgressBar.value = max_hp
+	
+	spellbook = Spellbook.new()
+	#spellbook.owner = self
+	add_child(spellbook)
+	for spell in starting_spells:
+		spellbook.add_spell(spell)
+	
 	
 func hit(damage):
 	hp -= damage;
@@ -32,7 +41,6 @@ func hit(damage):
 #		queue_free()
 	
 func _shoot_missile():
-		
 	var enemies = player_area.get_overlapping_bodies()
 	var closest_enemy;
 	var closest = INF
@@ -71,7 +79,25 @@ func _physics_process(delta):
 func _process(delta):
 	pass
 
-
 func _on_area_2d_area_exited(area):
 	if (area.is_in_group("spell")):
 		area.queue_free()
+		
+func get_spell_casting_position(spell_name):
+	return %StaffPosition.global_position
+	
+func get_spell_casting_direction(spell_name):
+	var enemies = player_area.get_overlapping_bodies()
+	var closest_enemy;
+	var closest = INF
+	var staff_pos = get_spell_casting_position(spell_name)
+	for enemy in enemies:
+		var enemy_dist = staff_pos.distance_to(enemy.global_position)
+		if (enemy_dist < closest):
+			closest = enemy_dist
+			closest_enemy = enemy
+				
+	if (closest_enemy):
+		return (closest_enemy.global_position - staff_pos).normalized()
+	else:
+		return Vector2(randf_range(-1., 1,), randf_range(-1., 1.)).normalized()				
