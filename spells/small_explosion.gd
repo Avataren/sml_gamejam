7a70ef41f30bc44997c08383c118a92a69bbc872
Morten Andersson <morten.andersson@gmail.com>
2024-04-13 12:44:19 +0200

    tweak explosion ease

--- spells/small_explosion.gd
+++ spells/small_explosion.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Particles.emitting = true
-	%PointLight2D.create_tween().tween_property(%PointLight2D,"texture_scale",0,0.5)
+	%PointLight2D.create_tween().tween_property(%PointLight2D,"texture_scale",0,0.5).set_ease(Tween.EASE_OUT)
 
 func _on_particles_finished():
 	queue_free()
